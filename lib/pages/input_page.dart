0f0e49d14563e2773f7d73faa3d1ad03be4aed7a
sohagmahin3 <sohagmahin3@gmail.com>
2020-01-07 17:03:16 +0600

    make input page responsive

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -10,111 +10,165 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 10),
-            child: Text(
-              'Choose your bus station',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-              ),
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    final double originalHeight = MediaQuery.of(context).size.height;
+    final double originalWidth = MediaQuery.of(context).size.width;
+
+    var labelTextPaddingPortraitMode =
+        EdgeInsets.only(top: originalHeight * 0.01, left: originalWidth * 0.02);
+    var labelTextPaddingLandscapeMode =
+        EdgeInsets.only(top: originalHeight * 0.01, left: originalWidth * 0.10);
+    var textFieldPaddingPortraitMode = EdgeInsets.only(
+        left: originalWidth * 0.02,
+        top: originalHeight * 0.025,
+        right: originalWidth * 0.10);
+    var textFieldPaddingLandscapeMode = EdgeInsets.only(
+        left: originalWidth * 0.10,
+        top: originalHeight * 0.02,
+        right: originalWidth * 0.15);
+
+    var busTileCardPaddingPortrait = EdgeInsets.only(
+        left: originalWidth * 0.03,
+        right: originalWidth * 0.03,
+        top: originalHeight * 0.001,
+        bottom: originalHeight * 0.01);
+    var busTileCardPaddingLandscape = EdgeInsets.only(
+        left: originalWidth * 0.05,
+        right: originalWidth * 0.05,
+        top: originalHeight * 0.001,
+        bottom: originalHeight * 0.01);
+
+    double labelTextFontSizePortrait = 20;
+    double labelTextFontSizeLandscape = 15;
+    double textFeildContainerHeightPortrait = 50.0;
+    double textFeildContainerHeightLandscape = 32.0;
+    double textFeildSizedBoxHeightPortrait = 10.0;
+    double textFeildSizedBoxHeightLandscape = 5.0;
+    double beforeBusListSizedBoxHeightPortrait = 15.0;
+    double beforeBusListSizedBoxHeightLandscape = 6.0;
+    double busTileContainerHeightPortrait = 60.0;
+    double busTileContainerHeightLandscape = 55.0;
+
+    print(isPortrait);
+    print(originalHeight);
+    print(originalWidth);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: isPortrait
+              ? labelTextPaddingPortraitMode
+              : labelTextPaddingLandscapeMode,
+          child: Text(
+            'Choose your bus station',
+            style: TextStyle(
+              fontSize: isPortrait
+                  ? labelTextFontSizePortrait
+                  : labelTextFontSizeLandscape,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0, top: 15.0, right: 40.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 50,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.my_location,
-                          size: 18,
-                          color: Colors.white,
-                        ),
-                        labelText: 'Source',
-                        filled: true,
-                        fillColor: Colors.white),
-                    style: TextStyle(
-                      backgroundColor: Colors.white,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-                  height: 50,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.location_on,
-                          size: 18,
-                          color: Colors.white,
-                        ),
-                        labelText: 'Destination',
-                        filled: true,
-                        fillColor: Colors.white),
-                    style: TextStyle(
-                      backgroundColor: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xffe1e2e1),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0)),
-                shape: BoxShape.rectangle,
-              ),
-              child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                          left: 10.0, right: 10.0, top: 8.0, bottom: 8.0),
-                      child: Container(
-                        height: 60,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.rectangle,
-                            borderRadius: BorderRadius.circular(10.0)),
-                        child: BusTile(
-                          name: '7 No Bus',
-                          type: 'Local',
-                          source: 'Gabtoli',
-                          destination: 'Zatrabari',
-                        ),
-//                        ListTile(
-//                          title: Text('7 No Bus'),
-//                          leading: Icon(Icons.directions_bus),
-//                          trailing: Text('Local'),
-//                          subtitle: Text('Gabtoli - Zatrabari'),
-//                          onTap: () {
-//                            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>BusDetails()));
-//                          },
-//                        ),
+        ),
+
+        //build source & destination text field
+        Padding(
+          padding: isPortrait
+              ? textFieldPaddingPortraitMode
+              : textFieldPaddingLandscapeMode,
+          child: Column(
+            children: <Widget>[
+              //source container
+              Container(
+                height: isPortrait
+                    ? textFeildContainerHeightPortrait
+                    : textFeildContainerHeightLandscape,
+                child: TextField(
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.my_location,
+                        size: 18,
+                        color: Colors.white,
                       ),
-                    );
-                  }),
-            ),
+                      labelText: 'Source',
+                      filled: true,
+                      fillColor: Colors.white),
+                  style: TextStyle(
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: isPortrait
+                    ? textFeildSizedBoxHeightPortrait
+                    : textFeildSizedBoxHeightLandscape,
+              ),
+              //Destination Container
+              Container(
+                height: isPortrait
+                    ? textFeildContainerHeightPortrait
+                    : textFeildContainerHeightLandscape,
+                child: TextField(
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.location_on,
+                        size: 18,
+                        color: Colors.white,
+                      ),
+                      labelText: 'Destination',
+                      filled: true,
+                      fillColor: Colors.white),
+                  style: TextStyle(
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: isPortrait
+              ? beforeBusListSizedBoxHeightPortrait
+              : beforeBusListSizedBoxHeightLandscape,
+        ),
+
+        //Build rectangle shape of box for displaying the bus list
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.only(top: originalHeight * 0.014),
+            decoration: BoxDecoration(
+              color: Color(0xffe1e2e1),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0)),
+              shape: BoxShape.rectangle,
+            ),
+            child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: busTileCardPaddingLandscape,
+                    child: Container(
+                      height: isPortrait
+                          ? busTileContainerHeightPortrait
+                          : busTileContainerHeightLandscape,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: BusTile(
+                        name: '7 No Bus',
+                        type: 'Local',
+                        source: 'Gabtoli',
+                        destination: 'Zatrabari',
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ),
+      ],
     );
   }
 }
