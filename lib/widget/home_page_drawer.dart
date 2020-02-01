5186c3c32ba45ffc7084a77437545f63a3ff4496
sohagmahin <sohagmahin@gmail.com>
2020-02-01 22:09:41 +0600

    Redesign

--- lib/widget/home_page_drawer.dart
+++ lib/widget/home_page_drawer.dart
@@ -15,21 +15,43 @@ class HomePageDrawer extends StatelessWidget {
                     bottom: MediaQuery.of(context).size.height * 0.01),
                 height: MediaQuery.of(context).size.height * 0.20,
                 alignment: Alignment.bottomLeft,
-                child: CircleAvatar(
-                  radius: MediaQuery.of(context).size.height * 0.040,
-                  backgroundImage: AssetImage("assets/images/bus_logo.jpg"),
-                  backgroundColor: Colors.indigoAccent,
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 3,
+                              spreadRadius: 5)
+                        ]),
+                    child: CircleAvatar(
+                      radius: MediaQuery.of(context).size.height * 0.06,
+                      backgroundImage: AssetImage("assets/images/logo.png"),
+                      // backgroundColor: Colors.indigoAccent,
+                    ),
+                  ),
                 ),
                 decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 1,
+                        spreadRadius: 1,
+                        offset: Offset(0, 2))
+                  ],
                   image: DecorationImage(
                     image: AssetImage("assets/images/bus_image.jpeg"),
                     fit: BoxFit.cover,
-                    colorFilter:
-                        ColorFilter.mode(Colors.white70, BlendMode.dstATop),
+                    colorFilter: ColorFilter.mode(
+                      Colors.white70,
+                      BlendMode.dstATop,
+                    ),
                   ),
                   shape: BoxShape.rectangle,
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(10.0),
+                    bottomLeft: Radius.circular(12.0),
+                    bottomRight: Radius.circular(12.0)
                   ),
                 ),
               ),
