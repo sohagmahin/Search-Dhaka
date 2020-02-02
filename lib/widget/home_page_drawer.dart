0b39604c48e26793838a2ddcc6c3850d43ae94fd
sohagmahin <sohagmahin@gmail.com>
2020-02-02 15:38:24 +0600

    build About Dialog

--- lib/widget/home_page_drawer.dart
+++ lib/widget/home_page_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePageDrawer extends StatelessWidget {
@@ -9,66 +10,89 @@ class HomePageDrawer extends StatelessWidget {
           color: Colors.white,
           child: Column(
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width * 0.01,
-                    bottom: MediaQuery.of(context).size.height * 0.01),
-                height: MediaQuery.of(context).size.height * 0.20,
-                alignment: Alignment.bottomLeft,
-                child: Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black54,
-                              blurRadius: 3,
-                              spreadRadius: 5)
-                        ]),
-                    child: CircleAvatar(
-                      radius: MediaQuery.of(context).size.height * 0.06,
-                      backgroundImage: AssetImage("assets/images/logo.png"),
-                      // backgroundColor: Colors.indigoAccent,
-                    ),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 1,
-                        spreadRadius: 1,
-                        offset: Offset(0, 2))
-                  ],
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/bus_image.jpeg"),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.white70,
-                      BlendMode.dstATop,
-                    ),
-                  ),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(12.0),
-                    bottomRight: Radius.circular(12.0)
-                  ),
-                ),
-              ),
+              _buildHeaderContainer(context),
               SizedBox(
                 height: 10,
               ),
-              ListTile(
-                title: Text(
-                  'About',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                leading: Icon(Icons.person_outline),
-                onTap: () {},
-              ),
+              _buildAboutListTile(context),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  ListTile _buildAboutListTile(BuildContext context) {
+    return ListTile(
+              title: Text(
+                'About',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              leading: Icon(Icons.person_outline),
+              onTap: () {
+                _buildCupertinoAlertDialog(context);
+              },
+            );
+  }
+
+  Container _buildHeaderContainer(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          left: MediaQuery.of(context).size.width * 0.01,
+          bottom: MediaQuery.of(context).size.height * 0.01),
+      height: MediaQuery.of(context).size.height * 0.20,
+      alignment: Alignment.bottomLeft,
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+            BoxShadow(color: Colors.black54, blurRadius: 3, spreadRadius: 5)
+          ]),
+          child: CircleAvatar(
+            radius: MediaQuery.of(context).size.height * 0.06,
+            backgroundImage: AssetImage("assets/images/logo.png"),
+            // backgroundColor: Colors.indigoAccent,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey,
+              blurRadius: 1,
+              spreadRadius: 1,
+              offset: Offset(0, 2))
+        ],
+        image: DecorationImage(
+          image: AssetImage("assets/images/bus_image.jpeg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.white70,
+            BlendMode.dstATop,
+          ),
+        ),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(12.0),
+            bottomRight: Radius.circular(12.0)),
+      ),
+    );
+  }
+
+  _buildCupertinoAlertDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text('About'),
+        content:
+            Text('\nBuild by Flutter\nCreated by SOHAG\nCredit: hasan,Soyab'),
+        actions: <Widget>[
+          CupertinoButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
       ),
     );
   }
