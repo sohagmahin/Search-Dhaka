43f44b1955bb2eb74efd5e5180afdd8e20549e10
sohagmahin <sohagmahin@gmail.com>
2020-02-02 19:06:25 +0600

    added exitDialog

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return ChangeNotifierProvider<BusListProvider>(
-      create: (context)=>BusListProvider(),
+      create: (context) => BusListProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Local Bus',
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'input_page.dart';
 import 'bus_list.dart';
@@ -17,54 +19,76 @@ class _HomePageState extends State<HomePage> {
   int selected = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Local Bus Dhaka route'),
-        centerTitle: true,
+    return WillPopScope(
+      onWillPop: () => exitDialog(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Local Bus Dhaka route'),
+          centerTitle: true,
+        ),
+        drawer: HomePageDrawer(),
+        backgroundColor: Colors.indigo, //  Color(0xff3f51b5)
+        bottomNavigationBar: BottomNavigationBar(
+          iconSize: 22.0,
+          backgroundColor: Colors.indigo,
+          items: [
+            BottomNavigationBarItem(
+                title: Text(
+                  'Home',
+                  style: kButtomTextStyle,
+                ),
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.grey,
+                ),
+                activeIcon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                )),
+            BottomNavigationBarItem(
+                title: Text(
+                  'All Bus',
+                  style: kButtomTextStyle,
+                ),
+                icon: Icon(
+                  Icons.format_line_spacing,
+                  color: Colors.grey,
+                ),
+                activeIcon: Icon(
+                  Icons.format_line_spacing,
+                  color: Colors.white,
+                ))
+          ],
+          currentIndex: selected,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: (select) {
+            setState(() {
+              selected = select;
+            });
+          },
+        ),
+        body: selected == 0 ? InputPage() : BusList(),
       ),
-      drawer: HomePageDrawer(),
-      backgroundColor: Colors.indigo, //  Color(0xff3f51b5)
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 22.0,
-        backgroundColor: Colors.indigo,
-        items: [
-          BottomNavigationBarItem(
-              title: Text(
-                'Home',
-                style: kButtomTextStyle,
-              ),
-              icon: Icon(
-                Icons.home,
-                color: Colors.grey,
-              ),
-              activeIcon: Icon(
-                Icons.home,
-                color: Colors.white,
-              )),
-          BottomNavigationBarItem(
-              title: Text(
-                'All Bus',
-                style: kButtomTextStyle,
-              ),
-              icon: Icon(
-                Icons.format_line_spacing,
-                color: Colors.grey,
-              ),
-              activeIcon: Icon(
-                Icons.format_line_spacing,
-                color: Colors.white,
-              ))
+    );
+  }
+
+  Future<bool> exitDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Do you want to exit'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('No'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text('Yes'),
+            onPressed: () => exit(0),
+          ),
         ],
-        currentIndex: selected,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        onTap: (select) {
-          setState(() {
-            selected = select;
-          });
-        },
       ),
-      body: selected == 0 ? InputPage() : BusList(),
     );
   }
 }
