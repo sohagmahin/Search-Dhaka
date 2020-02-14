3cf4f9e50edea3a03a10ea775455e776beed0c42
sohagmahin <sohagmahin@gmail.com>
2020-02-15 02:19:29 +0600

    Refactor the code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,73 +28,81 @@ class _HomePageState extends State<HomePage> {
           title: Text('Local Bus Dhaka route'),
           centerTitle: true,
           actions: <Widget>[
-            PopupMenuButton(
-              icon: Icon(Icons.more_vert),
-              onSelected: (status) {
-                if (status == FilterStatus.findNearestStop) {
-                  showDialog(
-                      context: context,
-                      builder: (ctx) => AlertDialog(
-                            content: Text('This feature will coming soon!'),
-                          ));
-                }
-              },
-              itemBuilder: (_) {
-                return [
-                  PopupMenuItem(
-                    child: Text('Find Nearest Stop'),
-                    value: FilterStatus.findNearestStop,
-                  ),
-                ];
-              },
-            )
+            _buildPopupMenuButton(context)
           ],
         ),
         drawer: HomePageDrawer(),
         backgroundColor: Colors.indigo, //  Color(0xff3f51b5)
-        bottomNavigationBar: BottomNavigationBar(
-          iconSize: 22.0,
-          backgroundColor: Colors.indigo,
-          items: [
-            BottomNavigationBarItem(
-                title: Text(
-                  'Home',
-                  style: kButtomTextStyle,
-                ),
-                icon: Icon(
-                  Icons.home,
-                  color: Colors.grey,
-                ),
-                activeIcon: Icon(
-                  Icons.home,
-                  color: Colors.white,
-                )),
-            BottomNavigationBarItem(
-                title: Text(
-                  'All Bus',
-                  style: kButtomTextStyle,
-                ),
-                icon: Icon(
-                  Icons.format_line_spacing,
-                  color: Colors.grey,
-                ),
-                activeIcon: Icon(
-                  Icons.format_line_spacing,
-                  color: Colors.white,
-                ))
-          ],
-          currentIndex: selected,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey,
-          onTap: (select) {
-            setState(() {
-              selected = select;
-            });
-          },
-        ),
+        bottomNavigationBar: _buildBottomNavigationBar(),
         body: selected == 0 ? InputPage() : AllBusList(),
       ),
     );
+  }
+
+  PopupMenuButton<FilterStatus> _buildPopupMenuButton(BuildContext context) {
+    return PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            onSelected: (status) {
+              if (status == FilterStatus.findNearestStop) {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          content: Text('This feature will coming soon!'),
+                        ));
+              }
+            },
+            itemBuilder: (_) {
+              return [
+                PopupMenuItem(
+                  child: Text('Find Nearest Stop'),
+                  value: FilterStatus.findNearestStop,
+                ),
+              ];
+            },
+          );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+        iconSize: 22.0,
+        backgroundColor: Colors.indigo,
+        items: [
+          BottomNavigationBarItem(
+              title: Text(
+                'Home',
+                style: kButtomTextStyle,
+              ),
+              icon: Icon(
+                Icons.home,
+                color: Colors.grey,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                color: Colors.white,
+              )),
+          BottomNavigationBarItem(
+              title: Text(
+                'All Bus',
+                style: kButtomTextStyle,
+              ),
+              icon: Icon(
+                Icons.format_line_spacing,
+                color: Colors.grey,
+              ),
+              activeIcon: Icon(
+                Icons.format_line_spacing,
+                color: Colors.white,
+              ))
+        ],
+        currentIndex: selected,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: (select) {
+          setState(() {
+            selected = select;
+          });
+        },
+      );
   }
 
   Future<bool> exitDialog(BuildContext context) {
